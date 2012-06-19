0ddaaf07a42f8533d8938b6b6357244e739830a4
Ozgun Ataman <ozataman@gmail.com>
2012-06-19 11:54:52 -0400

    Get working with snap 0.9

--- src/Snap/Extras/FormUtils.hs
+++ src/Snap/Extras/FormUtils.hs
@@ -1,7 +1,9 @@
-{-# LANGUAGE OverloadedStrings, RecordWildCards, NoMonomorphismRestriction #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE OverloadedStrings         #-}
+{-# LANGUAGE RecordWildCards           #-}
 
 module Snap.Extras.FormUtils
-    ( 
+    (
     -- * Transformers
       maybeTrans
     , readMayTrans
@@ -19,20 +21,15 @@ module Snap.Extras.FormUtils
 
 -------------------------------------------------------------------------------
 import           Control.Monad
-import qualified Data.ByteString.Char8       as B
-import           Data.List                   (find)
-import qualified Data.Map                    as M
+import qualified Data.ByteString.Char8 as B
+import           Data.List             (find)
+import qualified Data.Map              as M
 import           Data.Maybe
 import           Data.String
-import           Data.Text                   (Text)
-import qualified Data.Text                   as T
+import           Data.Text             (Text)
+import qualified Data.Text             as T
 import           Safe
 import           Snap.Core
-import           Text.Blaze
-import qualified Text.Blaze.Html5            as H
-import qualified Text.Blaze.Html5.Attributes as A
-import           Text.Blaze.Internal         (HtmlM(..))
-import           Text.Blaze.Renderer.XmlHtml
 import           Text.Digestive
 import           Text.Digestive.Snap
 import           Text.Templating.Heist
@@ -57,7 +54,7 @@ maybeTrans x = f x
 -------------------------------------------------------------------------------
 -- | Maybe read into target value
 readMayTrans
-    :: Read a 
+    :: Read a
     => Text -> Result v (Maybe a)
 readMayTrans x = Success $ readMay (T.unpack x)
 
@@ -66,9 +63,9 @@ readMayTrans x = Success $ readMay (T.unpack x)
 -- | Read into target value
 readTrans
     :: (Read a, IsString v)
-    => Text -> Result v a 
+    => Text -> Result v a
 readTrans a = maybe (Error "Unrecognized input") Success $ readMay . T.unpack $ a
-              
+
 
 
                               -------------------
@@ -79,12 +76,12 @@ readTrans a = maybe (Error "Unrecognized input") Success $ readMay . T.unpack $ 
 -- -------------------------------------------------------------------------------
 -- -- | Submit and cancel buttons with markup
 -- submitCancel
---     :: (Monad m, Functor m) 
---     => String 
+--     :: (Monad m, Functor m)
+--     => String
 --     -- ^ Submit button text
---     -> Text 
+--     -> Text
 --     -- ^ Cancel button text
---     -> Text 
+--     -> Text
 --     -- ^ Cancel button HREF
 --     -> Form m i e (FormHtml Html) ()
 -- submitCancel s ct cl = mapViewHtml enc (submit s)
@@ -97,8 +94,8 @@ readTrans a = maybe (Error "Unrecognized input") Success $ readMay . T.unpack $ 
 -- -------------------------------------------------------------------------------
 -- -- | Just a submit button with markup
 -- submitOnly
---     :: (Monad m, Functor m) 
---     => String 
+--     :: (Monad m, Functor m)
+--     => String
 --     -- ^ Submit button text
 --     -> Form m i e (FormHtml Html) ()
 -- submitOnly s = mapViewHtml enc (submit s)
@@ -108,31 +105,31 @@ readTrans a = maybe (Error "Unrecognized input") Success $ readMay . T.unpack $ 
 
 -- -------------------------------------------------------------------------------
 -- -- | Input form element wrapper to add a class of 'input'
--- inputFormat 
---     :: (Monad m, Functor m) 
---     => Form m i e (FormHtml Html) a 
+-- inputFormat
+--     :: (Monad m, Functor m)
+--     => Form m i e (FormHtml Html) a
 --     -> Form m i e (FormHtml Html) a
 -- inputFormat = mapViewHtml (H.div ! A.class_ "input")
 
 
 -- -------------------------------------------------------------------------------
 -- -- | Add a 'datepicker' classed div tag surrounding the form element
--- dateInput 
---     :: (Monad m, Functor m) 
---     => Form m i e (FormHtml Html) a 
+-- dateInput
+--     :: (Monad m, Functor m)
+--     => Form m i e (FormHtml Html) a
 --     -> Form m i e (FormHtml Html) a
 -- dateInput = mapViewHtml (H.div ! A.class_ "datepicker")
 
 
 -- -------------------------------------------------------------------------------
 -- -- | Render a form into a Heist splice
--- showForm 
---     :: Monad m 
---     => AttributeValue 
+-- showForm
+--     :: Monad m
+--     => AttributeValue
 --     -- ^ Form action target
---     -> AttributeValue 
+--     -> AttributeValue
 --     -- ^ Form submittion HTTP method
---     -> FormHtml (HtmlM a) 
+--     -> FormHtml (HtmlM a)
 --     -- ^ Form to display
 --     -> Splice m
 -- showForm act mth frm =
@@ -141,7 +138,7 @@ readTrans a = maybe (Error "Unrecognized input") Success $ readMay . T.unpack $ 
 --         frm' = H.form ! A.enctype (H.toValue $ show enctype) ! A.method mth
 --                       ! A.action act $ formHtml' >> return ()
 --    in return $ renderHtmlNodes frm'
-               
+
 
 
 -- -------------------------------------------------------------------------------
@@ -154,17 +151,17 @@ readTrans a = maybe (Error "Unrecognized input") Success $ readMay . T.unpack $ 
 -- -- Thing" automagically. It will:
 -- --
 -- -- 1. Look for the presence of a form namespace in Snap's 'Params'
--- -- 
+-- --
 -- -- 2. If it's there, it'll try to run the form and the views
--- -- 
+-- --
 -- -- 3. If no params are there, it'll assume that the form has not been
 -- -- submitted, so it should be a default form. It just renders the view
 -- -- in its default mode.
--- runViewForm' 
---   :: MonadSnap m 
---   => SnapForm m e t a 
+-- runViewForm'
+--   :: MonadSnap m
+--   => SnapForm m e t a
 --   -- ^ A digestive-functor form
---   -> String 
+--   -> String
 --   -- ^ Namespace used when rendering the form
 --   -> m (t, Maybe a)
 --   -- ^ The form's view and a possible form result
