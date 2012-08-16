7d98732fef3a3055fe192422251319995dd8a698
Mighty Byte <mightybyte@gmail.com>
2012-08-16 15:11:17 -0400

    Oops, need to export the new functions

--- src/Snap/Extras/CoreUtils.hs
+++ src/Snap/Extras/CoreUtils.hs
@@ -14,6 +14,8 @@ module Snap.Extras.CoreUtils
     , reqParam
     , readParam
     , readMayParam
+    , dirify
+    , undirify
     ) where
 
 -------------------------------------------------------------------------------
