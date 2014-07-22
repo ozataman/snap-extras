a09720999a8bcecfd31aede6033f3be0deff8730
Doug Beardsley <mightybyte@gmail.com>
2014-07-22 18:48:47 -0400

    Added a little howto at the top of PollStatus

--- src/Snap/Extras/PollStatus.hs
+++ src/Snap/Extras/PollStatus.hs
@@ -5,6 +5,49 @@
 {-# LANGUAGE ScopedTypeVariables       #-}
 {-# LANGUAGE TupleSections             #-}
 
+{-|
+
+This module provides infrastructure for polling the status of processes that
+run longer than a single request-response lifecycle.  There are two main
+components necessary to use this library: routes and splices.
+
+The routes are set up by the 'jobsHandler' function.  Typically you'll add it
+to your site's list of routes like this:
+
+> route [ ...
+>       , ("myJob", jobsHandler myJobStarter myJobStatusPage
+>                               "myJobUpdateStatusTemplate" ".jobStatusDiv")
+>       , ...
+>       ]
+
+You will also usually want to bind the three splices provided by this module.
+
+> splices = do
+>     ...
+>     "jobId" ## jobIdSplice
+>     "jobUpdateJs" ## updateSplice
+>     "myJobStatus" ## statusSplice getMyJobStatus
+
+You need to bind the last splice once for each type of action that you are
+polling, each with its own function for getting the job status.
+
+The third argument to jobsHandler is the template that will be rendered for
+status update requests.  It will typically be not much more than a
+@\<myJobStatus\>@ tag enclosing the custom markup for displaying your job status.
+Usually the last thing in that tag will be something like the following:
+
+> <ifNotFinished>
+>   <jobUpdateJs interval="500" jobId="${jobId}"/>
+> </ifNotFinished>
+
+This will poll for updates every 500 milliseconds.  Notice that we're using
+the jobId splice to get seamless handling of the job ID.  This is a
+convenient convention, but you can use a custom method for that if you want.
+See the documentation for 'statusSplice' for more information about the
+splices available to you inside the @\<myJobStatus\>@ tag.
+
+-}
+
 module Snap.Extras.PollStatus where
 
 ------------------------------------------------------------------------------
@@ -39,40 +82,8 @@ import           Snap.Extras.Ajax
 
 
 ------------------------------------------------------------------------------
-type JobId = Text
-
-
-------------------------------------------------------------------------------
-data JobState = Pending | Running | FinishedSuccess | FinishedFailure
-  deriving (Show,Eq)
-
-
-------------------------------------------------------------------------------
-isFinished :: JobState -> Bool
-isFinished FinishedSuccess = True
-isFinished FinishedFailure = True
-isFinished _ = False
-
-
-------------------------------------------------------------------------------
-data Status = Status
-    { srStartTime       :: UTCTime
-    , srJobState        :: JobState
-    , srEndTime         :: Maybe UTCTime
-    , srMessages        :: [Text]
-    , srAmountCompleted :: Double
-    , srAmountTotal     :: Double
-    }
-
-
-------------------------------------------------------------------------------
--- | Calculates the percent completed as an Int.
-statusPercentCompleted :: Status -> Int
-statusPercentCompleted Status{..} =
-    round $ 100.0 * srAmountCompleted / srAmountTotal
-
-
-------------------------------------------------------------------------------
+-- | Top-level handler that sets up routes for the job polling infrastructure.
+-- This handler sets up three sub-routes: start, status, and updateStatus.
 jobsHandler
     :: HasHeist b
     => Handler b b JobId
@@ -101,6 +112,7 @@ jobsHandler startJob statusPageHandler updateTemplate selector = do
 
 ------------------------------------------------------------------------------
 -- | A simple convenience splice to standardize the way templates get job IDs.
+-- This splice gets the job ID from the request param \"jobId\".
 jobIdSplice :: Splice (Handler b b)
 jobIdSplice = return $ yieldRuntimeText $ lift $ do
     bs <- getParam "jobId"
@@ -129,6 +141,66 @@ statusSplice getReport =
             mbs <- MaybeT $ getParam "jobId"
             MaybeT $ getReport $ decodeUtf8 mbs
         return $ fmap (ts,) sr
+
+
+------------------------------------------------------------------------------
+-- | Splice that inserts the javascript required to send an AJAX updateStatus
+-- request.  Takes two attributes:
+--
+-- interval - Delay in milliseconds before sending the request (default 1000)
+--
+-- jobId - Required attribute with the id of the job.  (Usually \"${jobId}\"
+-- should work fine.)
+updateSplice :: Splice (Handler b b)
+updateSplice = do
+    n <- getParamNode
+    runAttrs <- runAttributesRaw $ X.elementAttrs n
+    case X.getAttribute "jobId" n of
+      Nothing -> error $ T.unpack (X.elementTag n) ++ " must have a jobId attribute!"
+      _ -> return ()
+    return $ yieldRuntimeText $ do
+        attrs <- runAttrs
+        let delay :: Int = fromMaybe 1000 $ fromText =<< lookup "interval" attrs
+            jobId :: Text = fromMaybe "" $ lookup "jobId" attrs
+            js = updateJS jobId delay
+        return $ T.concat ["<script>", js, "</script>"]
+
+
+------------------------------------------------------------------------------
+type JobId = Text
+
+
+------------------------------------------------------------------------------
+-- | Enumeration of the states a job can be in.
+data JobState = Pending | Running | FinishedSuccess | FinishedFailure
+  deriving (Show,Eq)
+
+
+------------------------------------------------------------------------------
+-- | Returns a bool indicating whether the job is finished.
+isFinished :: JobState -> Bool
+isFinished FinishedSuccess = True
+isFinished FinishedFailure = True
+isFinished _ = False
+
+
+------------------------------------------------------------------------------
+-- | The complete status of a job.
+data Status = Status
+    { srStartTime       :: UTCTime
+    , srJobState        :: JobState
+    , srEndTime         :: Maybe UTCTime
+    , srMessages        :: [Text]
+    , srAmountCompleted :: Double
+    , srAmountTotal     :: Double
+    }
+
+
+------------------------------------------------------------------------------
+-- | Calculates the percent completed as an Int.
+statusPercentCompleted :: Status -> Int
+statusPercentCompleted Status{..} =
+    round $ 100.0 * srAmountCompleted / srAmountTotal
 
 
 ------------------------------------------------------------------------------
@@ -162,28 +234,6 @@ statusSplices = do
 ------------------------------------------------------------------------------
 getElapsed :: (UTCTime, Status) -> Int
 getElapsed (ts,sr) = round $ diffUTCTime ts (srStartTime sr)
-
-
-------------------------------------------------------------------------------
--- | Splice that inserts the javascript required to send an AJAX updateStatus
--- request.  Takes two attributes:
---
--- interval - Delay in milliseconds before sending the request (default 1000)
--- jobId - Required attribute with the id of the job.  (Usually "${jobId}"
--- should work fine.)
-updateSplice :: Splice (Handler b b)
-updateSplice = do
-    n <- getParamNode
-    runAttrs <- runAttributesRaw $ X.elementAttrs n
-    case X.getAttribute "jobId" n of
-      Nothing -> error $ T.unpack (X.elementTag n) ++ " must have a jobId attribute!"
-      _ -> return ()
-    return $ yieldRuntimeText $ do
-        attrs <- runAttrs
-        let delay :: Int = fromMaybe 1000 $ fromText =<< lookup "interval" attrs
-            jobId :: Text = fromMaybe "" $ lookup "jobId" attrs
-            js = updateJS jobId delay
-        return $ T.concat ["<script>", js, "</script>"]
 
 
 ------------------------------------------------------------------------------
